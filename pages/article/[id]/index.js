517c91bd6d1512af392da5e4be8832df91982b54
Garvnanwani <garvnanwani88@gmail.com>
2021-04-15 20:02:03 +0530

    added individual article page

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,9 +1,48 @@
-const article = () => {
+// import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const article = ({ article }) => {
+
+    // const router = useRouter()
+
+    // const { id } = router.query
+
     return (
-        <div>
-            This is an article
-        </div>
+        <>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+
+            <br />
+            <Link href='/'>Go Back</Link>
+        </>
     )
+}
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export const getStaticPaths = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+    const articles = await res.json()
+
+    const ids = articles.map(article => article.id)
+
+    const paths = ids.map(id => ({ params: { id: id.toString() } }))
+
+    return {
+        paths,
+        fallback: false
+    }
 }
 
 export default article
